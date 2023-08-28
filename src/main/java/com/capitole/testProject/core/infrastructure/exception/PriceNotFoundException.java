c6c4999be083d58f0f8f136e5a821fc79f0d4920
Candelaria Bertoldi <candelariabertoldi@gmail.com>
2023-08-28 13:50:59 -0300

    fix: Change date to string priceNotFoundException for beauty code message

--- src/main/java/com/capitole/testProject/core/infrastructure/exception/PriceNotFoundException.java
+++ src/main/java/com/capitole/testProject/core/infrastructure/exception/PriceNotFoundException.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class PriceNotFoundException extends NotFoundException {
 
-    public PriceNotFoundException(Date applicationDate, int brandId, int productId) {
+    public PriceNotFoundException(String applicationDate, int brandId, int productId) {
         super("No se encontro una lista de precios perteneciente a la fecha: "
                 + applicationDate + " para el pruducto ID: " + productId + " y cadena ID: " + brandId);
     }
